0cdd381b9c1e6147937c00956367d3f84ec035c8
Anwesh43 <anweshthecool0@gmail.com>
2017-04-28 06:56:16 +0530

    Adding ColorPalleteView

---
+++ colorpallete/src/main/java/com/anwesome/ui/colorpallete/ColorPalleteView.java
@@ -0,0 +1,21 @@
+package com.anwesome.ui.colorpallete;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by anweshmishra on 28/04/17.
+ */
+public class ColorPalleteView extends View{
+    public ColorPalleteView(Context context) {
+        super(context);
+    }
+    public void onDraw(Canvas canvas) {
+
+    }
+    public boolean onTouchEvent(MotionEvent event) {
+        return true;
+    }
+}
