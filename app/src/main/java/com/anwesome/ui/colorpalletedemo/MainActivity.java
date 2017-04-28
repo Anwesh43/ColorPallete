9e069dd278959bdbb987855012d99afc3e5ec188
Anwesh43 <anweshthecool0@gmail.com>
2017-04-28 09:04:23 +0530

    Using ColorPallete in app

--- app/src/main/java/com/anwesome/ui/colorpalletedemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/colorpalletedemo/MainActivity.java
@@ -3,11 +3,15 @@ package com.anwesome.ui.colorpalletedemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.anwesome.ui.colorpallete.ColorPallete;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ColorPallete colorPallete = new ColorPallete(this);
+        colorPallete.show();
     }
 }
